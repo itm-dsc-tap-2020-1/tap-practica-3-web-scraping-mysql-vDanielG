dcdf671174e69e156132a3cfbc87271fa010c4b8
vDanielG <53385358+vDanielG@users.noreply.github.com>
2020-03-12 11:55:24 -0600

    Add files via upload

--- RedPython.py
+++ RedPython.py
@@ -6,36 +6,32 @@ print("Ingresa una pagina web")
 Pag= input()
 
 print ("Python conectándose a MySQL") 
-conexion = mysql.connect ( host='localhost', user= 'root', passwd='', db='web' )
+conexion = mysql.connect ( host='localhost', user= 'root', passwd='', db='paginas' )
 operacion = conexion.cursor()
 print ("Conectado") 
 
+url=urlopen(Pag)
+bs = BeautifulSoup(url.read(),'html.parser')
+sql="INSERT INTO paginas_links (Enlace,Estatus) values(%s,%s)"
+for enlaces in bs.find_all("a"):
+    pag ="{}".format(enlaces.get("href"))
+    datos=(pag,False)
+    operacion.execute(sql,datos) 
+print("**********")
 
-class a:
-    def analizar(Pag):
-        url=urlopen(Pag)
-        bs = BeautifulSoup(url.read(),'html.parser')
-        sql="INSERT INTO LINK (link,status) values(%s,%s)"
-        for enlaces in bs.find_all("a"):
-            pag ="{}".format(enlaces.get("href"))
-            datos=(pag,False)
-            operacion.execute(sql,datos) 
-        print("**********")
+#op = a()
+#op.analizar(Pag)
 
-op = a()
-op.analizar(Pag)
+operacion.execute( "SELECT * FROM paginas_links" )
+for Enlaces,Estatus in operacion.fetchall():
+    print(Enlaces,Estatus)
 
-operacion.execute( "SELECT * FROM LINK" )
-for link,status in operacion.fetchall():
-    print(link,status)
+for Enlaces,Estatus in operacion.fetchall():
+    if(Estatus==0):
+        op.analizar(Enlaces)
+        datos=(Enlaces)
 
-sql1="UPDATE LINK SET status=True WHERE link=%s"
-for link,status in operacion.fetchall():
-    if(status==0):
-        op.analizar(link)
-        datos=(link)
-
-operacion.execute( "SELECT * FROM LINK" )
-for link,status in operacion.fetchall():
-    print(link,status)
+operacion.execute( "SELECT * FROM paginas_links" )
+for Enlaces,Estatus in operacion.fetchall():
+    print(Enlaces,Estatus)
 conexion.close()
